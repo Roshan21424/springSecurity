ca8b043f39f9488b7dd7c2c0f7712889f42707b4
RoshanRaj-M <roshanraj2142004@gmail.com>
2024-07-21 23:23:40 +0530

    basic setup

---
+++ basics/src/main/java/com/spring/securiy/controllers/ClientController.java
@@ -0,0 +1,14 @@
+package com.spring.securiy.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClientController {
+
+    @GetMapping("/hello")
+    public String hello(){
+       return "hello";
+
+    }
+}
